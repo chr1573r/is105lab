6cff8e75275d6d4085e4494b5893d32b6a9ef902
Christer Jonassen <christer@csdnserver.com>
2014-01-29 10:14:58 +0100

    test

--- gitklient.sh
+++ gitklient.sh
@@ -1,30 +1,85 @@
 #!/bin/bash
-#
+
 # gitklient.sh
-#
 # Kommandolinje klient for git(hub) - Christer Jonassen
-# Forutsetter at repositoryet er satt opp sikkelig
+# 
+# Forutsetter at current directory er et repository og at det er riktig satt opp.
 
 
+trap "{ echo "Fanget stopp signal, avslutter..."; sleep 1;  exit; }" SIGINT SIGTERM # Set trap for catching Ctrl-C and kills, so we can reset terminal upon exit
+trap "{ reset; echo \"gitklient terminated at `date`\"; }" EXIT # exit procedure
 
-Velg handling:
 
-1. Commit alle endringer
-   (git add * og git commit)
-
-2. Synkroniser med github.com
-   (git pull origin master og
-   	git push origin master)
-
+bekreft()
+{
+	read -n 1 "Trykk en tast for å fortsette..." SELECTION
+}
 
 leggtilogcommit()
 {
 	echo "'Utfører git add *'"
 	git add *
 
-	echo "Utfører git commit"
-	echo "Skriv inn kommentar til commiten du oppretter:"
-	git commit -m "Kommentar"
+	
+	read "Skriv inn kommentar til commiten du oppretter:" KOMMENTAR
+
+	echo "Vil bli commitet med kommentaren: $KOMMENTAR"
+	bekreft
+	echo "Utfører git commit!"
+	git commit -m "$KOMMENTAR"
 }
 
-synkronisermedgithub
+synkronisermedgithub()
+{
+	echo "Laster ned..."
+	echo "(git pull origin master)"
+	sleep 1
+	git pull origin master
+	echo "Laster opp..."
+	echo "(git push origin master)"
+	sleep 1
+	git push origin master
+}
+
+meny()
+{
+clear
+echo "Velg handling:"
+echo    
+echo "1. Commit alle endringer"
+echo "   (git add * og git commit)"
+echo    
+echo "2. Synkroniser med github.com"
+echo "   (git pull origin master og"
+echo "       git push origin master)"
+echo   
+echo "X. Avslutt"
+
+read -n 1 "Velg en handling (1, 2 eller X):" SELECTION
+
+case "$SELECTION" in
+ 	1)
+		leggtilogcommit
+		;;
+
+	2)
+		synkronisermedgithub
+		;;
+
+	x)
+		exit
+		;;
+
+	X)
+		exit
+		;;
+
+	*)
+		;;
+esac
+
+# selve runscriptet
+while true
+        do
+                meny
+        done
